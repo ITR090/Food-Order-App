0272ba6e5ae9300268bd2a3960f8c16484ba5b99
ITR090 <raghad.4309@gmail.com>
2023-04-19 00:24:55 +0300

    production build

--- src/components/UI/Button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Button = (props) => {
-    return (
-        <button type={props.btnType} className={props.btnStyle} onClick={props.click}>{props.children}</button>
-    )
-}
